9f14f25fdac87de79934a2305688c860731f2e44
snakerabbit <alison.s.cheng@gmail.com>
2018-04-13 23:22:00 -0700

    created initial routes

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ var app = express();
 var router = express.Router();
 var port = process.env.API_PORT || 3001;
 mongoose.connect('mongodb://user:password@ds243059.mlab.com:43059/eventgallery');
+var Twitter = require('twitter-node-client').Twitter;
+
+var config = {
+    "consumerKey": 'AstEiS2reCPdVYGdq3fN7lloW',
+    "consumerSecret": '30pksdC7Pvvd7PeFnkBmVwXlq2kU86nU93DeQkq4RMdU7kSyze',
+    "accessToken": '958834013782786048-dHtXAvSncPz1iny1sO6XyUuRYqQmXso',
+    "accessTokenSecret": 'muR2qPlfmPLGxl4D52MCZPjvBX1sSqFhKaleyFsyLCxGz'
+};
+
+var twitter = new Twitter(config);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -21,8 +31,23 @@ app.use(function(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-  res.json({message: 'API is up and running'});
+  res.json({message: 'this will be the main page'});
 });
+
+router.route('/posts')
+  .get(function(req,res) {
+    res.json({message: "this is where all the posts go"});
+  });
+router.route('/posts/:post_id')
+  .get(function(req, res) {
+    res.json({message: `post_id: ${req.params.post_id}`});
+  });
+
+router.route('/events')
+  .get(function(req, res){
+    res.json({message: "this is where all the events go"});
+  });
+
 
 app.use('/api', router);
 app.listen(port, function() {
